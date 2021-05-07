ac895484ccce53a947bc7dc4ae022d751f7c94a8
Nikhil Srivastava <nikhil.srivastava944@gmail.com>
2021-05-07 23:53:36 +0530

    Update AllZerosFirst.java

--- AllZerosFirst.java
+++ AllZerosFirst.java
@@ -6,7 +6,7 @@ public class Prac {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] arr={0,0,1,1,1,1,0};
+		int[] arr={0,0,1,1,1,1,0,0};
 		int j=arr.length-1;
 		for(int i=arr.length-1;i>=0;i--) {
 			if(arr[i]==1 && arr[j]==0) {
