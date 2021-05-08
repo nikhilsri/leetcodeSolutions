3299416adcf8bb18f987fa5930ab59c1b97d50d6
Nikhil Srivastava <nikhil.srivastava944@gmail.com>
2021-05-08 23:52:22 +0530

    Create Binary Tree Inorder Traversal.java
    Given the root of a binary tree, return the inorder traversal of its nodes' values.
    
     
    
    Example 1:
    
    
    Input: root = [1,null,2,3]
    Output: [1,3,2]
    Example 2:
    
    Input: root = []
    Output: []
    Example 3:
    
    Input: root = [1]
    Output: [1]
    Example 4:
    
    
    Input: root = [1,2]
    Output: [2,1]
    Example 5:
    
    
    Input: root = [1,null,2]
    Output: [1,2]
     
    
    Constraints:
    
    The number of nodes in the tree is in the range [0, 100].
    -100 <= Node.val <= 100
     
    
    Follow up:
    
    Recursive solution is trivial, could you do it iteratively?

---
+++ Binary Tree Inorder Traversal.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        List<Integer> res = new ArrayList<>();
+        if (root == null){
+            return res;
+        }
+       // TreeNode curr=root;
+       // System.out.println(curr.val);
+        while(root != null || !stack.isEmpty()) {
+                while(root != null) {
+                    stack.push(root);
+                    root = root.left;
+                }
+                root = stack.pop();
+                res.add(root.val);
+                root = root.right;
+                
+            }
+            return res;
+    }
+}
